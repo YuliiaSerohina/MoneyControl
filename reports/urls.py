6eb762e9b6b20acc613e535d4dcd9b8a8e3768a1
ju <Seregina.j.s@gmail.com>
2023-05-17 14:14:48 +0300

    v3

--- reports/urls.py
+++ reports/urls.py
@@ -3,8 +3,11 @@ import reports.views
 
 urlpatterns = [
     path('balance/', reports.views.get_cash_balance_report),
-    path('period_costs_pie_chart/', reports.views.select_period),
+    path('period_costs_pie_chart/', reports.views.select_period_costs_pie_chart),
     path('costs_pie_chart/', reports.views.get_costs_pie_chart),
-    path('cost/', reports.views.get_report_cost),
+    path('period_group_costs_pie_chart/', reports.views.select_period_group_costs_pie_chart),
+    path('group_costs_pie_chart/', reports.views.get_group_costs_pie_chart),
+    path('period_top_costs/', reports.views.select_period_top_costs),
+    path('top_costs/', reports.views.get_top_costs),
     path('', reports.views.get_all_reports)
 ]
--- reports/views.py
+++ reports/views.py
@@ -26,9 +26,9 @@ def get_cash_balance_report(request):
     return render(request, 'get_cash_balance_report.html', context)
 
 
-def select_period(request):
+def select_period_costs_pie_chart(request):
     user_name = request.user.username
-    return render(request, 'select_period.html', {"user_name": user_name})
+    return render(request, 'select_period_costs_pie_chart.html', {"user_name": user_name})
 
 
 def get_costs_pie_chart(request):
@@ -36,23 +36,83 @@ def get_costs_pie_chart(request):
     start_date = request.POST.get('date_start')
     end_date = request.POST.get('date_finish')
     costs_expense = Cost.objects.filter(user_id=request.user.pk, cost_group_id__type_group=0)
+    currency_all = Transactions.objects.filter(user_id=request.user.pk).values_list\
+        ('safe_id__currency', flat=True).distinct()
     total_costs_expense = []
-    for cost in costs_expense:
-        cost_sum = Transactions.objects.filter(
-            cost_id=cost.id, date__range=(start_date, end_date)).aggregate(total=Sum('sum_transactions'))
-        cost_sum = cost_sum['total'] or 0
-        if cost_sum:
-            total_costs_expense.append({cost: cost_sum})
-    context = {'user_name': user_name, 'total_costs_expense': total_costs_expense}
-    return render(request, '.html', context)      #доделать этот эндпоинт и сделать темплейт
+    for currency in currency_all:
+        one_cost_sum = []
+        for cost in costs_expense:
+            cost_sum = Transactions.objects.filter(
+                cost_id=cost.id, date__range=(start_date, end_date), safe_id__currency=currency).aggregate(total=Sum('sum_transactions'))
+            cost_sum = cost_sum['total'] or 0
+            if cost_sum:
+                one_cost_sum.append({cost: cost_sum})
+        total_costs_expense.append({currency: one_cost_sum})
+    context = {'user_name': user_name,
+               'total_costs_expense': total_costs_expense,
+               'start_date': start_date,
+               'end_date': end_date}
+    return render(request, 'get_costs_pie_chart.html', context)
+
+
+def select_period_group_costs_pie_chart(request):
+    user_name = request.user.username
+    return render(request, 'select_period_group_costs_pie_chart.html', {"user_name": user_name})
+
+
+def get_group_costs_pie_chart(request):
+    user_name = request.user.username
+    start_date = request.POST.get('date_start')
+    end_date = request.POST.get('date_finish')
+    group_costs_expense = CostGroup.objects.filter(user_id=request.user.pk, type_group=0)
+    currency_all = Transactions.objects.filter(user_id=request.user.pk).values_list\
+        ('safe_id__currency', flat=True).distinct()
+    total_group_costs_expense = []
+    for currency in currency_all:
+        one_group_cost_sum = []
+        for group_costs in group_costs_expense:
+            group_costs_sum = Transactions.objects.filter(
+                group_id=group_costs.id, date__range=(start_date, end_date),
+                safe_id__currency=currency).aggregate(total=Sum('sum_transactions'))
+            group_costs_sum = group_costs_sum['total'] or 0
+            if group_costs_sum:
+                one_group_cost_sum.append({group_costs: group_costs_sum})
+        total_group_costs_expense.append({currency: one_group_cost_sum})
+    context = {'user_name': user_name, 'start_date': start_date,
+               'end_date': end_date, 'total_group_costs_expense': total_group_costs_expense}
+    return render(request, 'get_group_costs_pie_chart.html', context)
+
+
+def select_period_top_costs(request):
+    user_name = request.user.username
+    return render(request, 'select_period_top_costs.html', {"user_name": user_name})
+
+
+def get_top_costs(request):
+    user_name = request.user.username
+    start_date = request.POST.get('date_start')
+    end_date = request.POST.get('date_finish')
+    costs_expense = Cost.objects.filter(user_id=request.user.pk, cost_group_id__type_group=0)
+    currency_all = Transactions.objects.filter(user_id=request.user.pk).values_list \
+        ('safe_id__currency', flat=True).distinct()
+    total_costs_expense = []
+    for currency in currency_all:
+        one_cost_sum = []
+        for cost in costs_expense:
+            cost_sum = Transactions.objects.filter(
+                cost_id=cost.id, date__range=(start_date, end_date), safe_id__currency=currency).aggregate(total=Sum('sum_transactions'))
+            cost_sum = cost_sum['total'] or 0
+            if cost_sum:
+                one_cost_sum.append({cost: cost_sum})
+        sorted_costs_list = sorted(one_cost_sum, key=lambda x: list(x.values())[0])
+        total_costs_expense.append({currency: sorted_costs_list})
+    context = {'user_name': user_name, 'total_costs_expense': total_costs_expense,
+               'start_date': start_date, 'end_date': end_date}
+    return render(request, 'get_top_costs.html', context)
 
 
 
 
-
-
-def get_report_cost(request):  # расходы по статьям
-    pass
 
 
 def get_all_reports(request):
